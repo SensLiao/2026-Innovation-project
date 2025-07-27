76fe12d703da9cc5731137852d00aaa25cc7f318
Karen Hu <kehu4196@uni.sydney.edu.au>
2025-07-27 19:09:48 +1000

    Figma interface intergrated, missing some format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import PatientPage from './pages/PatientPage';
-import ReportPage from './pages/ReportPage';
-// import Index from './pages/index';
+import PatientPage from '@/pages/PatientPage';
+import ReportPage from '@/pages/ReportPage';
+import Index from '@/pages/index';
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PatientPage />} />
-        <Route path="/report" element={<ReportPage />} />
-        {/* <Route path="/index" element={<Index />} /> */}
-      </Routes>
-    </BrowserRouter>
+  <TooltipProvider>
+  <Toaster />
+  <Sonner />
+  <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<PatientPage />} />
+    <Route path="/report" element={<ReportPage />} />
+    <Route path="/index" element={<Index />} />
+    </Routes>
+  </BrowserRouter>
+  </TooltipProvider>
+
   );
 }
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "../shared"),
     },
   },
 })
