78b96521bdd57c148deea577b6995bbbd5fb38cf
Karen Hu <kehu4196@uni.sydney.edu.au>
2025-08-17 15:17:26 +1000

    Signup, Patient delete, usePatient functions completed

--- frontend/src/useDB/useUsers.js
+++ frontend/src/useDB/useUsers.js
@@ -7,6 +7,16 @@ export const useUserDB = create((set, get) => ({
     users: [],
     loading: false,
     error: null,
+
+    signupData:{
+        name: '',
+        email: '',
+        phone: '',
+        password: '',
+    },
+
+    setSignupData: (signupData) => set({ signupData }),
+    resetSignupData: () => set({ signupData: { name: '', email: '', phone: '', password: '' } }),
     
     fetchUsers: async () => {
         set({ loading: true });
@@ -19,4 +29,48 @@ export const useUserDB = create((set, get) => ({
             set({ loading: false });
         }
     },
+
+    deleteUser: async (uid) => {
+        set({ loading: true });
+        try {
+            await axios.delete(`${BASE_URL}/api/users/${uid}`);
+            set((state) => ({
+                users: state.users.filter(user => user.uid !== uid),
+                error: null
+            }));
+        } catch (error) {
+            console.log("Error deleting user:", error);
+            set({ error: error.message });
+        } finally {
+            set({ loading: false });
+        }
+    },
+
+    addUser:async(e)=> {
+        e.preventDefault();
+        set({ loading: true });
+        try {
+            const { signupData } = get();
+            const passwordHash = btoa(signupData.password); // Simple base64 encoding for password
+            const newUser = {
+                name: signupData.name,
+                email: signupData.email,
+                phone: signupData.phone,
+                passwordhash: passwordHash,
+            };
+            // Send POST request to add user
+            await axios.post(`${BASE_URL}/api/users`, newUser);
+            
+            //Refetch data
+            await get().fetchUsers();
+            get().resetSignupData();
+            console.log("User added successfully");
+        } catch (error) {
+            console.log("Error adding user:", error);
+            set({ error: error.message });
+        } finally {
+            set({ loading: false });
+        }
+    }
+
 }));
